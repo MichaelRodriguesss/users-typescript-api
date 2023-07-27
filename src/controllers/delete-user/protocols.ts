354d5c0e5b4e032434893c9bed3ffa05fdd09afb
michael <michaeledavi12@gmail.com>
2023-07-27 12:03:40 -0300

    :feat: add DeleteUserController and it´s route

--- src/controllers/delete-user/protocols.ts
+++ src/controllers/delete-user/protocols.ts
@@ -1,4 +1,9 @@
 import { User } from "../../models/user";
+import { HttpRequest } from "../protocols";
+
+export interface IDeleteUserController {
+  handle(httpRequest: HttpRequest<any>): Promise<User>;
+}
 
 export interface IDeleteUserRepository {
   deleteUser(id: string): Promise<User>;
